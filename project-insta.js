b26f652df9cb10a21c95f2cb6eb223d4d7b10b1d
Hyunseok Lee <hxl5600@psu.edu>
2026-03-29 23:48:35 -0400

    Added JSON fetch, slide navigation, and localStorage likes functionality

--- project-insta.js
+++ project-insta.js
@@ -6,6 +6,8 @@ import { LitElement, html, css } from "lit";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
 import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
 import "./project-insta-slide.js";
+import "./project-insta-nav.js";
+import "./project-insta-dots.js";
 
 export class ProjectInsta extends DDDSuper(I18NMixin(LitElement)) {
   static get tag() {
@@ -18,12 +20,21 @@ export class ProjectInsta extends DDDSuper(I18NMixin(LitElement)) {
     this.t = {
       title: "Title",
     };
+
+    this.data = [];
+    this.index = 0;
+    this.slideCount = 0;
+    this.likes = [];
   }
 
   static get properties() {
     return {
       ...super.properties,
       title: { type: String },
+      data: { type: Array },
+      index: { type: Number },
+      slideCount: { type: Number },
+      likes: { type: Array },
     };
   }
 
@@ -44,6 +55,16 @@ export class ProjectInsta extends DDDSuper(I18NMixin(LitElement)) {
 
         .wrapper {
           width: 100%;
+          padding: 16px;
+          box-sizing: border-box;
+        }
+
+        .controls {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          gap: 12px;
+          margin-bottom: 16px;
         }
 
         @media (prefers-color-scheme: dark) {
@@ -56,10 +77,90 @@ export class ProjectInsta extends DDDSuper(I18NMixin(LitElement)) {
     ];
   }
 
+  async firstUpdated() {
+    const response = await fetch("./project-insta.json");
+    const json = await response.json();
+
+    this.data = json.data;
+    this.slideCount = this.data.length;
+    this.index = 0;
+
+    this.likes = this.data.map((_, i) => {
+      return localStorage.getItem("liked_" + i) === "true";
+    });
+  }
+
+  toggleLike(i) {
+    const updatedLikes = [...this.likes];
+    updatedLikes[i] = !updatedLikes[i];
+    this.likes = updatedLikes;
+    localStorage.setItem("liked_" + i, this.likes[i]);
+  }
+
+  next() {
+    if (this.index < this.slideCount - 1) {
+      this.index += 1;
+    }
+  }
+
+  prev() {
+    if (this.index > 0) {
+      this.index -= 1;
+    }
+  }
+
+  handleNavClick(e) {
+    if (e.detail.direction === "previous" || e.detail.direction === "prev") {
+      this.prev();
+    } else {
+      this.next();
+    }
+  }
+
+  handleDotClick(e) {
+    this.index = e.detail.index;
+  }
+
   render() {
+    if (!this.data.length) {
+      return html`<div class="wrapper">Loading...</div>`;
+    }
+
+    const item = this.data[this.index];
+
     return html`
       <div class="wrapper">
-        <slot></slot>
+        <div class="controls">
+          <play-list-nav
+            direction="previous"
+            .disabled=${this.index === 0}
+            @nav-click=${this.handleNavClick}
+          ></play-list-nav>
+
+          <project-insta-dots
+            .count=${this.slideCount}
+            .active=${this.index}
+            @dot-clicked=${this.handleDotClick}
+          ></project-insta-dots>
+
+          <play-list-nav
+            direction="next"
+            .disabled=${this.index === this.slideCount - 1}
+            @nav-click=${this.handleNavClick}
+          ></play-list-nav>
+        </div>
+
+        <project-insta-slide
+          insta-channel="${item.author.instaChannel}"
+          insta-username="${item.author.instaUsername}"
+          profile-pic="${item.author.profilePic}"
+          user-since="${item.author.userSince}"
+          .image="${item.image.fullsizeSource}"
+          insta-caption="${item.post.instaCaption}"
+          date-taken="${item.image.dateTaken}"
+          .liked=${this.likes[this.index]}
+          @toggle-like=${() => this.toggleLike(this.index)}
+        ></project-insta-slide>
       </div>
     `;
   }
